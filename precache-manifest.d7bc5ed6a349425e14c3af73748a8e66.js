da0bae500777cb84dc3aeb75f88749513d5a310f
unknown <snstotti@gmail.com>
2020-11-03 16:15:27 +0300

    Updates

---
+++ precache-manifest.d7bc5ed6a349425e14c3af73748a8e66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ce2c8071fb760434e6bb2490633d19",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "fe17905239cf3fbef16f",
+    "url": "/to-do/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "ea1fdc5568ee4468fa2a",
+    "url": "/to-do/static/css/main.b8a79623.chunk.css"
+  },
+  {
+    "revision": "fe17905239cf3fbef16f",
+    "url": "/to-do/static/js/2.978ae1fd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/to-do/static/js/2.978ae1fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea1fdc5568ee4468fa2a",
+    "url": "/to-do/static/js/main.fa40b1f5.chunk.js"
+  },
+  {
+    "revision": "bb957653ae216e45fce6",
+    "url": "/to-do/static/js/runtime-main.1b75bd56.js"
+  }
+]);
